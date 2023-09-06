c69e60327d4b86c755ced1d8980b24a3a425dbad
LittleCatXX <83004058+LittleCatXX@users.noreply.github.com>
2023-09-07 05:31:39 +0700

    update v2
    cleanfile
    change README

--- README.md
+++ README.md
@@ -1,45 +1,7 @@
-# thesis
-use with PHP XAMMP MYSQL line API
-- linked :  HOST-> Line API -> chatbot
-
-***road maps 07/2023***
--> need to update page / database finish 100%
--> need to update line API        finish 100%
-***road maps 08/2023***
--> have user login
-    -> add status
--> design web page
--> know where checkin
-    -> lab
-    -> room
-    -> library
-    -> toliet ? 
-
-
---8/17/2023
--improve system with replay messeges
--add bootstrap
--cleaning ignore file and funtion
-
-
-
-
-------------------
-    add API
--can save UID to DB
--can save text to DB
--can't reply
-------------
-้8/15/2023 
-
-**add**
-***- can link userID and UID with line messeng ***
-can scan and save data
-can messeg and reply with line API
-can save messeg to DB
-
-improve DB
--use DB Normaliztion
-add bidge table -> linkline
-
------------------
+***
+วิธีใช้งาน
+แก้ไข DB ในไฟล์ 
+    db_connection
+แก้ไข Token ในไฟล์
+    funtion.php -> สำหรับการส่งกลับข้อความถึงผู้ใช้
+    webhook.php -> สำหรับรับค่ากลับมาจาก line โดยการใช้ webhook ของ lineAPI
--- lastupdate07.sql
+++ lastupdate07.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 21, 2023 at 07:33 PM
+-- Generation Time: Sep 07, 2023 at 12:27 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -33,6 +33,34 @@ CREATE TABLE `checklistdata` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `checklistdata`
+--
+
+INSERT INTO `checklistdata` (`idchecklist`, `studentID`, `created_at`) VALUES
+(28, 63314784, '2023-08-10 10:41:03'),
+(30, 63314784, '2023-08-10 10:41:50'),
+(31, 63314784, '2023-08-10 11:22:01'),
+(32, 63314784, '2023-08-14 21:14:43'),
+(33, 63314784, '2023-08-15 06:01:33'),
+(37, 63314784, '2023-08-15 06:40:20'),
+(38, 63314784, '2023-08-15 06:42:48'),
+(39, 63314784, '2023-08-15 06:42:53'),
+(40, 63314784, '2023-08-15 06:44:46'),
+(41, 63314784, '2023-08-15 06:50:40'),
+(42, 63314784, '2023-08-16 22:28:41'),
+(43, 63314784, '2023-08-16 22:48:38'),
+(44, 63314784, '2023-08-16 22:52:55'),
+(45, 63314784, '2023-08-16 23:21:58'),
+(46, 63314784, '2023-08-16 23:24:01'),
+(47, 63314784, '2023-08-16 23:24:08'),
+(48, 63310601, '2023-08-17 17:15:48'),
+(49, 63310601, '2023-08-17 17:16:24'),
+(50, 63314784, '2023-08-17 17:16:37'),
+(51, 63314784, '2023-09-06 00:47:42'),
+(52, 63314784, '2023-09-06 00:48:38'),
+(53, 63314784, '2023-09-06 00:51:43');
+
 -- --------------------------------------------------------
 
 --
@@ -43,6 +71,14 @@ CREATE TABLE `linelink` (
   `user_id` varchar(255) NOT NULL,
   `studentID` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `linelink`
+--
+
+INSERT INTO `linelink` (`user_id`, `studentID`) VALUES
+('Ubc1a08a01162fc2a60b5fd11e34691d5', '63314784'),
+('Uea49568d95372578a27082ad832e114d', '63310601');
 
 -- --------------------------------------------------------
 
@@ -70,6 +106,15 @@ CREATE TABLE `students` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `studentID`, `firstName`, `lastName`) VALUES
+(6, 63314784, 'เหมียวๆ ทดสอบแก้ไข', 'มุแง้ว'),
+(7, 63314782, 'เหมียวๆ', 'มุแง้ว'),
+(10, 63310601, 'นายเขมชาติ', 'ตระกูลเลิศรัตน์');
+
+--
 -- Triggers `students`
 --
 DELIMITER $$
@@ -93,6 +138,25 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`) VALUES
+(1, '63314784', '123456');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `usersline`
 --
 
@@ -102,6 +166,17 @@ CREATE TABLE `usersline` (
   `message_content` text DEFAULT NULL,
   `created_at_line` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usersline`
+--
+
+INSERT INTO `usersline` (`id`, `user_id`, `message_content`, `created_at_line`) VALUES
+(3, 'Ubc1a08a01162fc2a60b5fd11e34691d5', 'ทดสอบบ', '2023-08-10 15:23:41'),
+(4, 'Uea49568d95372578a27082ad832e114d', 'Hi', '2023-08-10 15:26:38'),
+(5, 'Ubc1a08a01162fc2a60b5fd11e34691d5', 'เทส', '2023-08-14 20:10:33'),
+(6, 'Ubc1a08a01162fc2a60b5fd11e34691d5', '63314784', '2023-08-14 20:10:41'),
+(16, 'Uea49568d95372578a27082ad832e114d', '63310601', '2023-08-17 17:16:05');
 
 --
 -- Indexes for dumped tables
@@ -128,6 +203,12 @@ ALTER TABLE `students`
   ADD UNIQUE KEY `studentID` (`studentID`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `usersline`
 --
 ALTER TABLE `usersline`
@@ -141,25 +222,31 @@ ALTER TABLE `usersline`
 -- AUTO_INCREMENT for table `checklistdata`
 --
 ALTER TABLE `checklistdata`
-  MODIFY `idchecklist` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idchecklist` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `scanneddata`
 --
 ALTER TABLE `scanneddata`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `usersline`
 --
 ALTER TABLE `usersline`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
