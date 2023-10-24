cdca46efc5be7d0b8bdf7b7cb93c1b6d8d30876a
LittleCatXX <83004058+LittleCatXX@users.noreply.github.com>
2023-10-24 21:36:25 +0700

    Update stu_stat.php

--- stu_stat.php
+++ stu_stat.php
@@ -105,23 +105,23 @@ if (isset($_POST['startTimestamp']) && isset($_POST['endTimestamp'])) {
     <!-- ส่วนการแสดงกราฟ --> <br>
     <div class="row">
             <div class="col-md-6">
-    <canvas id="myChart" style="width: 25px; height: 25px;"></canvas>
+            <canvas id="myChart" style="width: 50%; height: auto;"></canvas>
     </div>
         </div> 
 
     <script>
         var ctx = document.getElementById('myChart').getContext('2d');
         var myChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'pie',
             data: {
                 labels: ['นักเรียนทั้งหมด', 'นักเรียนที่เช็คชื่อ', 'นักเรียนที่ขาด'],
                 datasets: [{
                     label: 'จำนวนคน',
                     data: [<?= $totalStudents ?>, <?= $checkedCount ?>, <?= $missingCount ?>],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(255, 206, 86, 0.2)'
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(60, 179, 113, 1)',
+                        'rgba(255, 206, 86, 1)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
