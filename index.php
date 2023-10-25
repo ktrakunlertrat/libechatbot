ab96552aeb0d52f94758a2640ab91362bbaf912d
ktrakunlertrat <ktrakunlertrat@gmail.com>
2023-10-25 07:37:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,6 @@
             <a class="nav-item nav-link" href="show_history.php">ประวัติการเข้าเรียน</a>
             <a class="nav-item nav-link" href="stu_stat.php">สถิติ</a>
             <a class="nav-item nav-link" href="page_addrole.php">เพิ่มบทบาท</a>
-            <a class="nav-item nav-link" href="stu_stat.php">กราฟ </a>
             <a class="nav-item nav-link" href="logout.php">Logout</a>
                
             </div>
